79f884d027e42f57e0c3a45673ccc7240ef756be
nefisegll <nefise.gullu.tr@gmail.com>
2023-04-03 00:46:05 +0300

    Unit Test codes were written for Longest Common Subsequence.

--- ce100-hw2-sln/ce100-hw2-algo-test-cs/UnitTest1.cs
+++ ce100-hw2-sln/ce100-hw2-algo-test-cs/UnitTest1.cs
@@ -59,15 +59,15 @@ namespace ce100_hw2_algo_test_cs
     {
         [TestMethod]
         public void TestMatrixChainMultiplicationDP_BestCase()
-        {            
+        {
             int[] matrixDimensionArray = new int[] { 30, 35, 15, 5, 10, 20, 25 };
             string expectedMatrixOrder = "((A1 (A2 A3)) ((A4 A5) A6))";
             int expectedOperationCount = 40;
-            
+
             string matrixOrder = "";
             int operationCount = 0;
             int result = MatrixChainMultiplicationDP.Mcmdp(matrixDimensionArray, ref matrixOrder, ref operationCount);
-            
+
             Assert.AreEqual(expectedMatrixOrder, matrixOrder);
             Assert.AreEqual(expectedOperationCount, operationCount);
             Assert.AreEqual(15125, result);
@@ -75,15 +75,15 @@ namespace ce100_hw2_algo_test_cs
 
         [TestMethod]
         public void TestMatrixChainMultiplicationDP_WorstCase()
-        {           
+        {
             int[] matrixDimensionArray = new int[] { 10, 20, 30, 40, 50 };
             string expectedMatrixOrder = "(((A1 A2) A3) A4)";
             int expectedOperationCount = 13;
-            
+
             string matrixOrder = "";
             int operationCount = 0;
             int result = MatrixChainMultiplicationDP.Mcmdp(matrixDimensionArray, ref matrixOrder, ref operationCount);
-            
+
             Assert.AreEqual(expectedMatrixOrder, matrixOrder);
             Assert.AreEqual(expectedOperationCount, operationCount);
             Assert.AreEqual(38000, result);
@@ -91,15 +91,15 @@ namespace ce100_hw2_algo_test_cs
 
         [TestMethod]
         public void TestMatrixChainMultiplicationDP_AverageCase()
-        {            
+        {
             int[] matrixDimensionArray = new int[] { 5, 10, 3, 12, 5, 50 };
             string expectedMatrixOrder = "(((A1 A2) (A3 A4)) A5)";
             int expectedOperationCount = 24;
-            
+
             string matrixOrder = "";
             int operationCount = 0;
             int result = MatrixChainMultiplicationDP.Mcmdp(matrixDimensionArray, ref matrixOrder, ref operationCount);
-            
+
             Assert.AreEqual(expectedMatrixOrder, matrixOrder);
             Assert.AreEqual(expectedOperationCount, operationCount);
             Assert.AreEqual(1655, result);
@@ -161,12 +161,70 @@ namespace ce100_hw2_algo_test_cs
         }
     }
 
+    [TestClass]
+    public class LongestCommonSubsequenceTests
+    {
+        [TestMethod]
+        public void Lcs_BestCase_ShouldReturnExpectedResult()
+        {
+            string inputArray1 = "abcd";
+            string inputArray2 = "abcd";
+            string expectedOutputLcs = "abcd";
+            int expectedOutputLcslength = 4;
 
+            string outputLcs;
+            int outputLcslength;
+            int result = LongestCommonSubsequence.Lcs(inputArray1, inputArray2, out outputLcs, out outputLcslength);
 
+            Assert.AreEqual(expectedOutputLcslength, outputLcslength);
+            Assert.AreEqual(expectedOutputLcs, outputLcs);
+            Assert.AreEqual(0, result);
+        }
 
+        [TestMethod]
+        public void Lcs_WorstCase_ShouldReturnExpectedResult()
+        {
+            string inputArray1 = "a";
+            string inputArray2 = "b";
+            string expectedOutputLcs = "";
+            int expectedOutputLcslength = 0;
 
+            string outputLcs;
+            int outputLcslength;
+            int result = LongestCommonSubsequence.Lcs(inputArray1, inputArray2, out outputLcs, out outputLcslength);
 
+            Assert.AreEqual(expectedOutputLcslength, outputLcslength);
+            Assert.AreEqual(expectedOutputLcs, outputLcs);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Lcs_AverageCase_ShouldReturnExpectedResult()
+        {
+            string inputArray1 = "AGGTAB";
+            string inputArray2 = "GXTXAYB";
+            string expectedOutputLcs = "GTAB";
+            int expectedOutputLcslength = 4;
+
+            string outputLcs;
+            int outputLcslength;
+            int result = LongestCommonSubsequence.Lcs(inputArray1, inputArray2, out outputLcs, out outputLcslength);
+
+            Assert.AreEqual(expectedOutputLcslength, outputLcslength);
+            Assert.AreEqual(expectedOutputLcs, outputLcs);
+            Assert.AreEqual(0, result);
+        }
+    }
 
 
 
 }
+
+
+
+
+
+
+
+
+
