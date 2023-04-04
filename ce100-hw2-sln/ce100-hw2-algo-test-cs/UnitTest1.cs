aaf7bed4e746dcd64a9a5d6b7df8e6af71c19686
nefisegll <nefise.gullu.tr@gmail.com>
2023-04-04 10:38:00 +0300

    Arrangements have been made.

--- ce100-hw2-sln/ce100-hw2-algo-test-cs/UnitTest1.cs
+++ ce100-hw2-sln/ce100-hw2-algo-test-cs/UnitTest1.cs
@@ -7,16 +7,13 @@ namespace ce100_hw2_algo_test_cs
     {
         [TestMethod]
         public void TestHeapSortWithBestCaseInput()
-        {
-            // Arrange
+        {          
             int[] inputArray = { 1, 2, 3, 4, 5 };
             int[] expectedOutput = { 1, 2, 3, 4, 5 };
             int[] outputArray = new int[inputArray.Length];
-
-            // Act
+            
             int result = HeapSortAlgorithm.HeapSort(inputArray, ref outputArray, enableDebug: false);
-
-            // Assert
+            
             Assert.AreEqual(0, result);
             CollectionAssert.AreEqual(expectedOutput, outputArray);
         }
@@ -24,31 +21,25 @@ namespace ce100_hw2_algo_test_cs
         [TestMethod]
         public void TestHeapSortWithWorstCaseInput()
         {
-            // Arrange
             int[] inputArray = { 5, 4, 3, 2, 1 };
             int[] expectedOutput = { 1, 2, 3, 4, 5 };
             int[] outputArray = new int[inputArray.Length];
-
-            // Act
+            
             int result = HeapSortAlgorithm.HeapSort(inputArray, ref outputArray, enableDebug: false);
-
-            // Assert
+           
             Assert.AreEqual(0, result);
             CollectionAssert.AreEqual(expectedOutput, outputArray);
         }
 
         [TestMethod]
         public void TestHeapSortWithAverageCaseInput()
-        {
-            // Arrange
+        {           
             int[] inputArray = { 3, 5, 2, 1, 4 };
             int[] expectedOutput = { 1, 2, 3, 4, 5 };
             int[] outputArray = new int[inputArray.Length];
-
-            // Act
+           
             int result = HeapSortAlgorithm.HeapSort(inputArray, ref outputArray, enableDebug: false);
-
-            // Assert
+            
             Assert.AreEqual(0, result);
             CollectionAssert.AreEqual(expectedOutput, outputArray);
         }
@@ -111,16 +102,13 @@ namespace ce100_hw2_algo_test_cs
     {
         [TestMethod]
         public void TestMCMRemBestCase()
-        {
-            // Arrange
+        {            
             int[] matrixDimensionArray = { 30, 35, 15, 5, 10, 20, 25 };
             int operationCount = 0;
             string matrixOrder = null;
-
-            // Act
+           
             int result = MatrixChainMultiplicationMemorizedRec.mcmrem(matrixDimensionArray, ref matrixOrder, ref operationCount);
-
-            // Assert
+            
             Assert.AreEqual(0, result);
             Assert.AreEqual("((A1.(A2.A3)).((A4.A5).A6))", matrixOrder);
             Assert.AreEqual(15125, operationCount);
@@ -128,16 +116,13 @@ namespace ce100_hw2_algo_test_cs
 
         [TestMethod]
         public void TestMCMRemWorstCase()
-        {
-            // Arrange
+        {            
             int[] matrixDimensionArray = { 10, 20, 30, 40, 50, 60 };
             int operationCount = 0;
             string matrixOrder = null;
 
-            // Act
             int result = MatrixChainMultiplicationMemorizedRec.mcmrem(matrixDimensionArray, ref matrixOrder, ref operationCount);
-
-            // Assert
+            
             Assert.AreEqual(0, result);
             Assert.AreEqual("((((A1.A2).A3).A4).A5)", matrixOrder);
             Assert.AreEqual(68000, operationCount);
@@ -146,15 +131,12 @@ namespace ce100_hw2_algo_test_cs
         [TestMethod]
         public void TestMCMRemAverageCase()
         {
-            // Arrange
             int[] matrixDimensionArray = { 20, 30, 40, 10, 5, 30 };
             int operationCount = 0;
             string matrixOrder = null;
-
-            // Act
+         
             int result = MatrixChainMultiplicationMemorizedRec.mcmrem(matrixDimensionArray, ref matrixOrder, ref operationCount);
-
-            // Assert
+            
             Assert.AreEqual(0, result);
             Assert.AreEqual("((A1.(A2.(A3.A4))).A5)", matrixOrder);
             Assert.AreEqual(14000, operationCount);
@@ -166,19 +148,16 @@ namespace ce100_hw2_algo_test_cs
     {
         [TestMethod]
         public void Lcs_BestCase_ReturnsCorrectLcsLengthAndString()
-        {
-            // Arrange
+        {            
             string input1 = "abcde";
             string input2 = "abcde";
             string expectedLcs = "abcde";
             int expectedLcsLength = 5;
-
-            // Act
+            
             string actualLcs;
             int actualLcsLength;
             int result = LongestCommonSubsequence.Lcs(input1, input2, out actualLcs, out actualLcsLength);
 
-            // Assert
             Assert.AreEqual(expectedLcs, actualLcs);
             Assert.AreEqual(expectedLcsLength, actualLcsLength);
             Assert.AreEqual(0, result);
@@ -186,19 +165,16 @@ namespace ce100_hw2_algo_test_cs
 
         [TestMethod]
         public void Lcs_WorstCase_ReturnsCorrectLcsLengthAndString()
-        {
-            // Arrange
+        {            
             string input1 = "abcdefghijklmnopqrstuvwxyz";
             string input2 = "zyxwvutsrqponmlkjihgfedcba";
             string expectedLcs = "z";
             int expectedLcsLength = 1;
-
-            // Act
+            
             string actualLcs;
             int actualLcsLength;
             int result = LongestCommonSubsequence.Lcs(input1, input2, out actualLcs, out actualLcsLength);
-
-            // Assert
+            
             Assert.AreEqual(expectedLcs, actualLcs);
             Assert.AreEqual(expectedLcsLength, actualLcsLength);
             Assert.AreEqual(0, result);
@@ -206,19 +182,16 @@ namespace ce100_hw2_algo_test_cs
 
         [TestMethod]
         public void Lcs_AverageCase_ReturnsCorrectLcsLengthAndString()
-        {
-            // Arrange
+        {            
             string input1 = "abcdxyzefg";
             string input2 = "hijkxyzbklm";
             string expectedLcs = "xyz";
             int expectedLcsLength = 3;
 
-            // Act
             string actualLcs;
             int actualLcsLength;
             int result = LongestCommonSubsequence.Lcs(input1, input2, out actualLcs, out actualLcsLength);
-
-            // Assert
+           
             Assert.AreEqual(expectedLcs, actualLcs);
             Assert.AreEqual(expectedLcsLength, actualLcsLength);
             Assert.AreEqual(0, result);
